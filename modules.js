96a83b9a71dc0f34e61ce749c725334d9852ca52
Ed Rooth <ed.rooth@rackspace.com>
2013-09-20 14:31:42 -0700

    tabs -> spaces

--- modules.js
+++ modules.js
@@ -1,18 +1,18 @@
 var fs = require("fs");
 
 function modules(app) {
-	console.log("loading modules");
-	var directory = "./modules/";
-	fs.readdir(directory, function(err, files) {
-		if(err) {
-			console.error("Skipping modiles:", err);
-			return;
-		}
-		files.forEach(function(file) {
-			console.log("loading module:", file);
-			require(directory + file)(app);
-		});
-	})
+  console.log("loading modules");
+  var directory = "./modules/";
+  fs.readdir(directory, function(err, files) {
+    if(err) {
+      console.error("Skipping modiles:", err);
+      return;
+    }
+    files.forEach(function(file) {
+      console.log("loading module:", file);
+      require(directory + file)(app);
+    });
+  });
 }
 
 module.exports = modules;
