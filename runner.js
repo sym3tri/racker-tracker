9426326799a0047f4029c29a804dc724892ebe5e
Ed Rooth <ed.rooth@rackspace.com>
2013-10-04 17:31:52 -0700

    tabs to spaces

--- runner.js
+++ runner.js
@@ -1,76 +1,75 @@
 var fs = require('fs'),
-	config = JSON.parse(fs.readFileSync('./config.json', 'utf8')),
-	models = require('./models')(config).models;
+    config = JSON.parse(fs.readFileSync('./config.json', 'utf8')),
+    models = require('./models')(config).models;
 
 require('datejs');
 
 function pad(s) {
-	return s < 10 ? '0' + s : s;
+  return s < 10 ? '0' + s : s;
 }
 
 function toSqlDate(date) {
   return date.getUTCFullYear() + '-' +
-		pad(date.getUTCMonth()+1) + '-' +
-		pad(date.getUTCDate()) + ' ' +
-		pad(date.getUTCHours()) + ':' +
-		pad(date.getUTCMinutes()) + ':' +
-		pad(date.getUTCSeconds());
+    pad(date.getUTCMonth()+1) + '-' +
+    pad(date.getUTCDate()) + ' ' +
+    pad(date.getUTCHours()) + ':' +
+    pad(date.getUTCMinutes()) + ':' +
+    pad(date.getUTCSeconds());
 }
 
 
 require('./modules')(config)
 .then(function(modules) {
-	var User = models.User,
-		Stats = models.Stats;
+  var User = models.User,
+      Stats = models.Stats;
 
-	var end_date = Date.today();
+  var end_date = Date.today();
 
-	User.findAll().success(function(users) {
-		if(!users) {
-			return;
-		}
-		users.forEach(function(user) {
-			if(!user.service) {
-				return;
-			}
-			Stats.find({
-				'where': {'userid': user.id },
-				'order': 'date DESC'
-			})
-			.success(function(latest_stat) {
-				var start_date = Date.today().addDays(-7);
+  User.findAll().success(function(users) {
+    if(!users) {
+      return;
+    }
+    users.forEach(function(user) {
+      if(!user.service) {
+        return;
+      }
+      Stats.find({
+        'where': {'userid': user.id },
+        'order': 'date DESC'
+      })
+      .success(function(latest_stat) {
+        var start_date = Date.today().addDays(-7);
 
-				if(latest_stat) {
-					start_date = latest_stat.date;
-				}
+        if(latest_stat) {
+          start_date = latest_stat.date;
+        }
 
-				console.log('calling fetch: start:', start_date, 'end:', end_date, 'user', user.id);
-				modules[user.service].fetch(user, start_date, end_date)
-					.then(function(days) {
-						days.forEach(function(day) {
-							try {
-							Stats.find({ 'where': {
-								'userid': user.id,
-								'date': toSqlDate(day.date)
-							}})
-							.success(function(stat) {
-								if(!stat) {
-									stat = Stats.build({
-										'userid': user.id,
-										'date': day.date
-									});
-								}
-
-								stat.calories = day.calories;
-								stat.steps = day.steps;
-								stat.save();
-							});
-							} catch(e) {
-								console.error('error caught:', e, e.lineNumber);
-							}
-						});
-					});
-			});
-		});
-	});
+        console.log('calling fetch: start:', start_date, 'end:', end_date, 'user', user.id);
+        modules[user.service].fetch(user, start_date, end_date)
+          .then(function(days) {
+            days.forEach(function(day) {
+              try {
+              Stats.find({ 'where': {
+                'userid': user.id,
+                'date': toSqlDate(day.date)
+              }})
+              .success(function(stat) {
+                if(!stat) {
+                  stat = Stats.build({
+                    'userid': user.id,
+                    'date': day.date
+                  });
+                }
+                stat.calories = day.calories;
+                stat.steps = day.steps;
+                stat.save();
+              });
+              } catch(e) {
+                console.error('error caught:', e, e.lineNumber);
+              }
+            });
+          });
+      });
+    });
+  });
 });
